9d90b9c682e1601418b699d9cb6c487e28f1c2b2
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-16 16:01:05 +0100

    feat: add state management to react component

--- templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
@@ -1,5 +1,4 @@
 
-
 <% if (props && props.length > 0) {%>
 export interface <%- capitalize(dashToCamel(name)) %>Props {
 <% props.forEach(p => {%>
@@ -8,8 +7,35 @@ export interface <%- capitalize(dashToCamel(name)) %>Props {
 }
 <%}%>
 
+<% if (state && state.length > 4 ) { %>
+interface State {
+    <% state.forEach(s => {%>
+        <%- s %>
+    <% }) %>
+}
+
+const reducer = (state: State, action) => {
+  switch (action.type) {
+      <% state.map(item => item.split(':')).map(([k, v]) => { %>
+      case "SET_<%- k.toUpperCase()  %>":
+          return { ...state, <%- k %>: action.payload}
+      <% }) %>
+      default return state;
+  }
+
+}
+<% } %>
+
 export const <%- capitalize(dashToCamel(name)) %> = (
     <% if (props && props.length > 0 ) { %>
         props: <%- capitalize(dashToCamel(name)) %>Props
     <% } %>
-) => <div />
+) => {
+    <% if (state && state.length > 4 ) { %>
+    const [state, dispatch] = React.useReducer(reducer);
+    <% } else if (state) { %>
+        <% state.map(s => s.split(':')).forEach(([k,v]) => {%>
+        const [ <%- k  %>, set<%- capitalize(k) %>] = useState()
+        <% }) %>
+    <% } %>
+<div />}
