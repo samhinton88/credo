2e3cdff2c689ce46b86ae6340127e268ab089c43
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-16 15:33:45 +0100

    fix: change react to ts and add props

---
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
@@ -0,0 +1,11 @@
+
+
+<% if (props) {%>
+export interface <%- capitalize(dashToCamel(name)) %>Props {
+<% props.forEach(p => {%>
+    <% p %>
+<% }) %>
+}
+<%}%>
+
+export const <%- capitalize(dashToCamel(name)) %> = () => <div />
