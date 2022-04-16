1749ca365c6a9840e531e6bf46bebc595fa0414d
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-16 15:14:13 +0100

    fix: spacing on react test

--- templates/reactComponentGroup/<%- name %>/<%- name %>-component.test.jsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.test.jsx
@@ -2,9 +2,10 @@ import { <%- capitalize(dashToCamel(name)) %> } from './<%- name %>-component'
 
 describe("<<%- capitalize(dashToCamel(name)) %> />", () => {
 <% if (tests) { %>
-<% tests.forEach(t => { %>
-    it("should <%- t %>", () => {
+<% tests.forEach(t => { 
+    %>it("should <%- t %>", () => {
 
     })<% }) %>
+    
 <% } %>
 })
