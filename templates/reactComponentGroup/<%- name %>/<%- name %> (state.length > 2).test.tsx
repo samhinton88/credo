c1244e5ab38462bd7b1ff79528f9bcf00964d7e8
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-19 14:07:12 +0100

    fix: rm extra brackets

--- templates/reactComponentGroup/<%- name %>/<%- name %> (state.length > 2).test.tsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %> (state.length > 2).test.tsx
@@ -1,14 +1,14 @@
 import { reducer, <%- capitalize(dashToCamel(name)) %>State } from './<%- name %>-component';
 
 describe('reducer', () => {
-            <% state.map(item => item.split(':')).forEach(([k, v]) => { %>
-              it('should change state', () => {
-        
-                const newState = reducer([], { type: "SET_<%- k.split('-').join('_').toUpperCase()  %>", payload: null });
+<% state.map(item => item.split(':')).forEach(([k, v]) => { %>
+        it('should change state', () => {
+  
+          const newState = reducer([], { type: "SET_<%- k.split('-').join('_').toUpperCase()  %>", payload: null });
 
-                expect(newState.<%-  dashToCamel(k) %>).toEqual(null)
-              });
-            <% }) %>
-        }
-      }
+          expect(newState.<%-  dashToCamel(k) %>).toEqual(null)
+        });
+<% }) %>
+        
+      
 })
