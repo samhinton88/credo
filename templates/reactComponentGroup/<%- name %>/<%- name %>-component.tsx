c5a60454520a56c09fd71895f91e462938845f85
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-19 13:43:59 +0100

    fix: state name

--- templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
@@ -16,7 +16,7 @@ export interface <%- capitalize(dashToCamel(name)) %>State {
  * For components that require more than a few different pieces
  * of state to manage, we prefer useReducer
  */
-export const reducer = (state: State, action) => {
+export const reducer = (state: <%- capitalize(dashToCamel(name)) %>State, action) => {
   switch (action.type) {
       <% state.map(item => item.split(':')).map(([k, v]) => { %>
       case "SET_<%- k.split('-').join('_').toUpperCase()  %>":
