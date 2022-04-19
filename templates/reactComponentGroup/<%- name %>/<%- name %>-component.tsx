c8dc856f54bbf53622c3772bc214b44d8143a388
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-19 13:35:09 +0100

    chore: export reducer and reducer state

--- templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.tsx
@@ -7,7 +7,7 @@ export interface <%- capitalize(dashToCamel(name)) %>Props {
 <%}%>
 
 <% if (state && state.length > 4 ) { %>
-interface State {
+export interface <%- capitalize(dashToCamel(name)) %>State {
     <% state.forEach(s => {%>
         <%- s %><% }) %>
 }
@@ -16,7 +16,7 @@ interface State {
  * For components that require more than a few different pieces
  * of state to manage, we prefer useReducer
  */
-const reducer = (state: State, action) => {
+export const reducer = (state: State, action) => {
   switch (action.type) {
       <% state.map(item => item.split(':')).map(([k, v]) => { %>
       case "SET_<%- k.toUpperCase()  %>":
