0493243809218dc93ee73453608fe3d716986a32
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-19 14:22:39 +0100

    feat: add fetchy stuff to test

--- templates/reactComponentGroup/<%- name %>/<%- name %>-component.test.tsx
+++ templates/reactComponentGroup/<%- name %>/<%- name %>-component.test.tsx
@@ -1,4 +1,27 @@
-import { <%- capitalize(dashToCamel(name)) %> } from './<%- name %>-component'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+<% if (fetchedResourcePath) { %>import { setupServer, rest } from 'msw'<%}%>
+import { <%- capitalize(dashToCamel(name)) %> } from "./<%- name %>-component"
+
+const getMock = jest.fn();
+
+<% if (fetchedResourcePath) { %>
+const server = setupServer(
+  rest.get('/<%- fetchedResourcePath %>', (req, res, ctx) =>  {
+    getMock()
+    return res(ctx.json({ }))
+  }),
+)
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
+
+<%}%>
 
 describe("<<%- capitalize(dashToCamel(name)) %> />", () => {
 <% if (tests) { %>
